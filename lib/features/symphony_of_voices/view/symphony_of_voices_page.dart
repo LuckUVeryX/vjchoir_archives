f4d36d3b3cb824f03713b2c7eaec46e28bd7dd7e
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-10-04 22:04:29 +0800

    Update SOV tab UI

--- lib/features/symphony_of_voices/view/symphony_of_voices_page.dart
+++ lib/features/symphony_of_voices/view/symphony_of_voices_page.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:vjchoir_archives/app/router.dart';
 import 'package:vjchoir_archives/features/symphony_of_voices/controllers/controllers.dart';
 import 'package:vjchoir_archives/utils/utils.dart';
@@ -16,33 +15,26 @@ class SymphonyOfVoicesPage extends ConsumerWidget {
 
     return symphonyOfVoices.buildWhen(
       data: (data) {
-        return MasonryGridView.count(
-          crossAxisCount: 2,
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
           itemCount: data.sov.length,
           itemBuilder: (context, index) {
             final sov = data.sov[index];
-            return FilledCard(
-              child: Stack(
-                children: [
-                  InkWell(
-                    onTap: () => context.go('${Routes.sov}/${sov.id}'),
-                    child: Column(
-                      children: [
-                        CachedNetworkImage(
-                          imageUrl: sov.artwork,
-                          placeholder: (_, __) =>
-                              const ShimmerPlaceholder(aspectRatio: 1),
-                        ),
-                        ListTile(
-                          title: Text(sov.abbr),
-                          subtitle: Text(sov.title),
-                          dense: true,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+            return ImageCard(
+              image: ImageWithTitle(
+                image: CachedNetworkImage(
+                  imageUrl: sov.artwork,
+                  placeholder: (_, __) =>
+                      const ShimmerPlaceholder(aspectRatio: 1),
+                ),
+                listTile: ListTile(
+                  dense: true,
+                  title: Text(sov.abbr),
+                ),
               ),
+              onTap: () => context.go('${Routes.sov}/${sov.id}'),
             );
           },
         );

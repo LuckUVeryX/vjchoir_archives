6635bfbdbbd3e388a74de4f6f193381b59862c19
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-10-07 14:56:40 +0800

    Fix failing test

--- lib/app/dark_theme_controller.dart
+++ lib/app/dark_theme_controller.dart
@@ -1,23 +1,26 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shake/shake.dart';
 import 'package:vjchoir_archives/features/storage/storage.dart';
 
 final darkThemeProvider =
     StateNotifierProvider<DarkThemeController, bool>((ref) {
-  return DarkThemeController(ref.watch(preferencesRepositoryProvider));
+  return DarkThemeController(ref.watch(preferencesRepositoryProvider)).._init();
 });
 
 class DarkThemeController extends StateNotifier<bool> {
   DarkThemeController(
     this._repo,
-  ) : super(_repo.kDarkMode) {
-    _detector = ShakeDetector.autoStart(onPhoneShake: toggle);
-  }
+  ) : super(_repo.kDarkMode);
 
   final PreferencesRepository _repo;
   late final ShakeDetector _detector;
 
+  void _init() {
+    _detector = ShakeDetector.autoStart(onPhoneShake: toggle);
+  }
+
+  @visibleForTesting
   void toggle() {
     state = !state;
     _repo.kDarkMode = state;
